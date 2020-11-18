c1dbc4847af5015f215937f58a1ab9ca05cdd4d3
Jack Pendleton <jacklrpendleton@gmail.com>
2020-11-18 14:39:15 -0600

    Push working compat layer

--- Program.cs
+++ Program.cs
@@ -1,26 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading;
 using Driver.Corsair;
-using Driver.HyperXAlloy.RGB;
-using Driver.PhillipsHue;
-using Driver.Razer;
-using GameIntegration.Destiny2PC;
+using Driver.RGBnet;
+using Source.RainbowWave;
+
 //you can either use nuget to get simple led or add the project to this solution. Solution will be in GIT as nuget.
 using SimpleLed;
-using Source.gridripple;
-using Source.SimpleCycle;
-using SteelSeriesSLSProvider;
 
 namespace MadLedSDK
 {
     class Program
     {
+        static List<ControlDevice> devices = new List<ControlDevice>();
+        static Dictionary<int, ControlDevice> driv = new Dictionary<int, ControlDevice>();
+
+        static void ShowDevices()
+        {
+            try
+            {
+                driv = new Dictionary<int, ControlDevice>();
+                int ct = 1;
+
+                foreach (var controlDevice in devices.ToArray())
+                {
+                    Console.WriteLine(ct + ": " + controlDevice.Driver.Name() + "-" + controlDevice.Name + " - " +
+                                      controlDevice.DeviceType + ", has configUI: " +
+                                      (controlDevice.Driver is ISimpleLedWithConfig));
+                    driv.Add(ct, controlDevice);
+
+                    ct++;
+                }
+            }
+            catch
+            {
+            }
+
+        }
+
         static void Main(string[] args)
         {
+
+            var apiClient = new SimpleLedApiClient();
+            var driverProps = apiClient.GetProductsByCategory(ProductCategory.Effect).Result;
+
             try
             {
                 Directory.CreateDirectory("SLSConfigs");
@@ -34,32 +61,26 @@ namespace MadLedSDK
             
             
             SLSManager ledManager = new SLSManager("SLSConfigs");
+
+            ledManager.DeviceAdded += LedManager_DeviceAdded;
+            ledManager.DeviceRemoved += LedManager_DeviceRemoved;
             //Add drivers manually like the example below.
             //you wll need to add the driver csproj too.
             //you will need to add at LEAST two - one for source, one for dest
             //ledManager.Drivers.Add(new CUEDriver());
-            ledManager.Drivers.Add(new RazerDriver());
-            ledManager.Drivers.Add(new SteelSeriesDriver());
-            ledManager.Drivers.Add(new DriverHyperXAlloyRGB());
-            ledManager.Drivers.Add(new GridRipple());
-            //ledManager.Drivers.Add(new Destiny2());
-            ledManager.Drivers.Add(new SimpleRGBCycleDriver());
+            
+            ledManager.Drivers.Add(new RainbowWaveDriver());
+
+            ledManager.Drivers.Add(new RGBNetDriver());
+
             ledManager.RescanRequired += LedManager_RescanRequired;
             
             ledManager.Init();
             Console.WriteLine("Getting devices");
-            List<ControlDevice> devices = ledManager.GetDevices();
+            
 
-            Dictionary<int, ControlDevice> driv = new Dictionary<int, ControlDevice>();
-            int ct = 1;
-            foreach (var controlDevice in devices)
-            {
-                Console.WriteLine(ct + ": " + controlDevice.Driver.Name() + "-" + controlDevice.Name + " - " + controlDevice.DeviceType + ", " + controlDevice.Driver.GetProperties().Author);
-                driv.Add(ct, controlDevice);
-
-                ct++;
-            }
-
+            
+           
             string derp = "";
             ControlDevice cycleFan = null;
 
@@ -107,6 +128,20 @@ namespace MadLedSDK
 
             }
             
+        }
+
+        private static void LedManager_DeviceRemoved(object sender, Events.DeviceChangeEventArgs e)
+        {
+            Console.WriteLine("Device Removed: " + e.ControlDevice.Name);
+            devices.Remove(e.ControlDevice);
+            ShowDevices();
+        }
+
+        private static void LedManager_DeviceAdded(object sender, Events.DeviceChangeEventArgs e)
+        {
+            Console.WriteLine("Device Added: "+e.ControlDevice.Name);
+            devices.Add(e.ControlDevice);
+            ShowDevices();
         }
 
         private static void LedManager_RescanRequired(object sender, EventArgs e)
